64f2cdfa1957c67056dbe40b79537fa850179626
Dylan Cree <dylanpab1@gmail.com>
2025-11-08 15:35:44 +0000

    init commit

---
+++ src/main/java/com/lootchat/LootChat/LootChatApplication.java
@@ -0,0 +1,13 @@
+package com.lootchat.LootChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LootChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LootChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lootchat/LootChat/LootChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lootchat.LootChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LootChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
