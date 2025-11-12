e7c3e61cc62a5561459ff3db8a7488cbe13971b9
VOIDAppie <alperen.elcik@student.hu.nl>
2025-11-12 12:00:43 +0100

    refactored: the utils class to take the architecture of PaginationRequest for easier readability

--- src/main/java/com/lootchat/LootChat/util/PaginationUtils.java
+++ src/main/java/com/lootchat/LootChat/util/PaginationUtils.java
@@ -1,5 +1,6 @@
 package com.lootchat.LootChat.util;
 
+import com.lootchat.LootChat.dto.PaginationRequest;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -9,7 +10,7 @@ import org.springframework.data.domain.Sort;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PaginationUtils {
 
-    public static Pageable getPagable(Integer page, Integer size, Sort.Direction direction, String sortField) {
-        return PageRequest.of(page, size, direction, sortField);
+    public static Pageable getPagable(PaginationRequest request) {
+        return PageRequest.of(request.getPage(), request.getSize(), request.getDirection(), request.getSortField());
     }
 }
