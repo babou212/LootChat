35d26aff38fc648cb1a3c4341252bddc02583c4d
Dylan Cree <dylanpab1@gmail.com>
2025-11-15 16:11:43 +0000

    fix: small refactor

--- src/main/java/com/lootchat/LootChat/security/SecurityConfig.java
+++ src/main/java/com/lootchat/LootChat/security/SecurityConfig.java
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -41,16 +40,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new RegexRequestMatcher("/api/invites/.*/register", "POST")).permitAll()
-                        .requestMatchers("/api/invites/*/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/invites/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/invites/**").permitAll()
-                        .requestMatchers("/ws/**").permitAll()
-                        .anyRequest().authenticated()
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.POST, "/api/invites/*/register").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/invites/**").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+            .requestMatchers("/ws/**").permitAll()
+            .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
