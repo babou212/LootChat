bc58e91003a8ac8c4b02e0c549d4e5038132ce12
Dylan Cree <dylanpab1@gmail.com>
2025-11-22 15:22:03 +0000

    Add cache control headers and fix redirect loop

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -91,7 +91,7 @@ export default defineNuxtConfig({
             'style-src \'self\' \'unsafe-inline\'',
             'img-src \'self\' data: https: blob:',
             'font-src \'self\' data:',
-            'connect-src \'self\' http://localhost:8080 ws://localhost:8080 ws: wss: https://tenor.googleapis.com',
+            'connect-src \'self\' http://localhost:8080 ws://localhost:8080 ws: wss: https://tenor.googleapis.com https://api.iconify.design',
             'media-src \'self\' https:',
             'frame-src \'self\' https://www.youtube.com https://www.youtube-nocookie.com',
             'worker-src \'self\' blob:',
