b7408e34744eccedfd6c53b1b6e84575ea8e97d8
Dylan Cree <dylanpab1@gmail.com>
2025-11-23 20:51:25 +0000

    fix: mock s3

--- src/test/java/com/lootchat/LootChat/LootChatApplicationTests.java
+++ src/test/java/com/lootchat/LootChat/LootChatApplicationTests.java
@@ -2,12 +2,20 @@ package com.lootchat.LootChat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import com.lootchat.LootChat.service.S3FileStorageService;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LootChatApplicationTests {
+
+	@MockBean
+	private S3FileStorageService s3FileStorageService;
 
 	@Test
 	void contextLoads() {
+		// Context loads successfully with mocked S3 service
 	}
 
 }
