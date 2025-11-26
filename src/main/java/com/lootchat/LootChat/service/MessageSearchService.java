704f4b69ffe5e641f5f4cc21ff44c35b8f683a2e
VOIDAppie <alperen.elcik@student.hu.nl>
2025-11-26 16:06:42 +0100

    added: full text search function

--- src/main/java/com/lootchat/LootChat/service/MessageSearchService.java
+++ src/main/java/com/lootchat/LootChat/service/MessageSearchService.java
@@ -6,7 +6,13 @@ import com.lootchat.LootChat.repository.MessageSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -49,5 +55,11 @@ public class MessageSearchService {
         } catch (Exception e) {
             log.error("Failed to delete message from elasticsearch with id{}", messageId);
         }
+    }
+
+    public List<MessageDocument> searchAllMessages(String query, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.Direction.DESC, "CreatedAt");
+        Page<MessageDocument> results = messageSearchRepository.findMessageContaining(query, pageable);
+        return results.getContent();
     }
 }
