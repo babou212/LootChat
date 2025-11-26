f8b1f13e150490e5860ab3300eb9793154964f04
VOIDAppie <alperen.elcik@student.hu.nl>
2025-11-26 14:56:24 +0100

    added: repository for elasticsearch

---
+++ src/main/java/com/lootchat/LootChat/repository/MessageSearchRepository.java
@@ -0,0 +1,26 @@
+package com.lootchat.LootChat.repository;
+
+import com.lootchat.LootChat.entity.MessageDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageSearchRepository extends ElasticsearchRepository<MessageDocument, String> {
+
+    //full text search
+    Page<MessageDocument> findMessageContaining(String content, Pageable pageable);
+
+    //search by channel
+    Page<MessageDocument> findChannelIdContainingMessage(Long channelId, String content, Pageable pageable);
+
+    //search by user
+    Page<MessageDocument> findUserIdContainingMessage(Long userId, String content, Pageable pageable);
+
+    // find the OG messageID in database
+    MessageDocument findByMessageId(Long messageId);
+
+    // delete by message Id
+    void deleteByMessageId(Long messageId);
+}
